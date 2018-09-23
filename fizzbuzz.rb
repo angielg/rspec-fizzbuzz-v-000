9d8e75fdb0bfc2d1397f765c184f29cfccda883b
Angie Goza <angielg9888@gmail.com>
2018-09-23 23:35:06 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,18 +6,20 @@ def fizzbuzz(num)
  
   if num % 3 == 0 && num % 5 == 0
     return "FizzBuzz"
+  end
     
 
   else if num % 5 == 0
     return "Buzz"
+  end
     
 
   else if num % 3 == 0
     return "Fizz"
+  end
     
   else num
     return nil
   end
 end
-end
-end
+
