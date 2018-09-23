c66d99eb4535f28beb41d1b3cfd193471db47d1c
Angie Goza <angielg9888@gmail.com>
2018-09-23 22:48:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+ 
+  if num % 3 == 0
+    return "Fizz"
+  end
+
+  else if num % 5 == 0
+    return "Buzz"
+  end
+
+  else num % 3 && 5 == 0
+    return "Fizzbuzz"
+  end
+
+end
+
